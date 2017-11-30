70d3506a054901f23fb003a4fad420f773fa03d4
Jady Urbina <jady.urbina@upr.edu>
2017-11-30 14:13:28 -0400

    Added the dao for resources

---
+++ dao/resource.py
@@ -0,0 +1,53 @@
+from config.dbconfig import pg_config
+import psycopg2
+
+class ResourceDAO:
+    def __init__(self):
+
+        connection_url = "dbname=%s user=%s password=%s" % (pg_config['dbname'],
+                                                            pg_config['user'],
+                                                            pg_config['passwd'])
+        self.conn = psycopg2._connect(connection_url)
+
+    def getAllResources(self):
+        cursor = self.conn.cursor()
+        query = "select * from resource;"
+        cursor.execute(query)
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getResourceById(self, r_id):
+            cursor = self.conn.cursor()
+            query = "select * from resource where r_id = %s;"
+            cursor.execute(query, (r_id,))
+            result = cursor.fetchone()
+            return result
+
+    def getResourceByCategory(self, r_id):
+            cursor = self.conn.cursor()
+            query = "select * from resource where r_category = %s;"
+            cursor.execute(query, (r_id,))
+            result = []
+            for row in cursor:
+                result.append(row)
+            return result
+
+    def getUserSuppliersByResourceId(self, r_id):
+        cursor = self.conn.cursor()
+        query = "select u_id, u_name, u_lastname, u_email, u_region, u_phone, u_age from user natural inner join supplier natural inner join resource where r_id = %s;"
+        cursor.execute(query, (r_id,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getUserConsumersByResourceId(self, r_id):
+        cursor = self.conn.cursor()
+        query = "select u_id, u_name, u_lastname, u_email, u_region, u_phone, u_age from user natural inner join consumer natural inner join resource where r_id = %s;"
+        cursor.execute(query, (r_id,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result

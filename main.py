7f8ab82af8d47b4de05219767300ad6906a0a9fb
Jady Urbina <jady.urbina@upr.edu>
2017-11-30 21:55:24 -0400

    Added the routes for the Seller Users

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from handler.resource import ResourceHandler
+from handler.seller import SellerHandler
 
 app = Flask(__name__)
 
@@ -25,6 +26,21 @@ def getPartsBySuplierId(r_id):
 @app.route('/DisasterApp/resources/<int:r_id>/buyers')
 def getPartsBySuplierId(r_id):
     return ResourceHandler().getBuyersByResourceId(r_id)
+
+@app.route('/DisasterApp/sellers')
+def getAllSuppliers():
+    if not request.args:
+        return SellerHandler().getAllSellers()
+    else:
+        return SellerHandler().searchSeller(request.args)
+
+@app.route('/Disaster/sellers/<int:s_id>')
+def getSellerById(s_id):
+    return SellerHandler().getSupplierById(s_id)
+
+@app.route('/DisasterApp/sellers/<int:s_id>/parts')
+def getResourcesBySellerId(s_id):
+    return SellerHandler().getResourcesBySellerId(s_id)
 
 if __name__=='__main__':
     app.run()

fdde7b87bf6834e08c67671ebd4f3482c39ca2cb
Jady Urbina <jady.urbina@upr.edu>
2018-01-08 20:32:42 -0400

    Added the types and categories for resources

--- handler/transaction.py
+++ handler/transaction.py
@@ -50,8 +50,58 @@ class TransactionHandler:
         result = {}
         result['r_id'] = row[0]
         result['r_name'] = row[1]
-        result['r_category'] = row[2]
-        result['r_type'] = row[3]
+        if(row[2]==1):
+            result['r_category'] = "Water"
+            if (row[3]==1):
+                result['r_type'] = "Bottled Water"
+            elif(row[3]==2):
+                result['r_type'] = "1 Gallon Water"
+        elif (row[2] == 2):
+            result['r_category'] = "Fuel"
+            if (row[3]==1):
+                result['r_type'] = "Diesel"
+            elif(row[3]==2):
+                result['r_type'] = "Gasoline"
+            else:
+                result['r_type'] = "Propane"
+        elif (row[2] == 3):
+            result['r_category'] = "Baby Food"
+            result['r_type'] = "Baby Food"
+        elif (row[2] == 4):
+            result['r_category'] = "Medications"
+            result['r_type'] = "Medications"
+        elif (row[2] == 5):
+            result['r_category'] = "Canned Food"
+            result['r_type'] = "Canned Food"
+        elif (row[2] == 6):
+            result['r_category'] = "Dry Food"
+            result['r_type'] = "Dry Food"
+        elif (row[2] == 7):
+            result['r_category'] = "Ice"
+            result['r_type'] = "Ice"
+        elif (row[2] == 8):
+            result['r_category'] = "Medical Devices"
+            result['r_type'] = "Medical Devices"
+        elif (row[2] == 9):
+            result['r_category'] = "Heavy Equipment"
+            result['r_type'] = "Heavy Equipment"
+        elif (row[2] == 10):
+            result['r_category'] = "Tools"
+            result['r_type'] = "Tools"
+        elif (row[2] == 11):
+            result['r_category'] = "Clothing"
+            result['r_type'] = "Clothing"
+        elif (row[2] == 12):
+            result['r_category'] = "Batteries"
+            result['r_type'] = "Batteries"
+        else:
+            result['r_category'] = "Power Generators"
+            if (row[3]==1):
+                result['r_type'] = "Diesel Power Generator"
+            elif(row[3]==2):
+                result['r_type'] = "Gasoline Power Generator"
+            else:
+                result['r_type'] = "Propane Power Generator"
         return result
 
     def build_creditcard_dict(self, row):
